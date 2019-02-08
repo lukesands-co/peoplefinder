6652a149dacaf6fbc7abeb703cfeb7130e1a17ff
Luke S <luke.sands@cabinetoffice.gov.uk>
2019-02-08 16:22:55 +0000

    Change SMTP authentication type for AWS SES

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -16,7 +16,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address: ENV['SMTP_HOST'],
     port: '587',
-    authentication: :plain,
+    authentication: :login,
     user_name: ENV['SMTP_USERNAME'],
     password: ENV['SMTP_PASSWORD'],
     domain: ENV['SMTP_DOMAIN'],
