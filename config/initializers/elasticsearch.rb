d78da0601eabd808151e6d7e6b37d31b38d3e0d7
Luke S <luke.sands@cabinetoffice.gov.uk>
2019-02-08 14:06:45 +0000

    Set es request type

--- config/initializers/elasticsearch.rb
+++ config/initializers/elasticsearch.rb
@@ -1,20 +1,19 @@
-if Rails.env.production? or Rails.env.staging?
-  require 'faraday_middleware/aws_signers_v4'
+require 'faraday_middleware/aws_signers_v4'
 
-  transport = Elasticsearch::Transport::Transport::HTTP::Faraday.new(
-    hosts: [{host: ENV['ELASTICSEARCH_HOST'], port: ENV['ELASTICSEARCH_PORT']}]
-  ) do |faraday|
-    faraday.request :aws_signers_v4,
+transport = Elasticsearch::Transport::Transport::HTTP::Faraday.new(
+  hosts: [{host: ENV['ELASTICSEARCH_HOST'], port: ENV['ELASTICSEARCH_PORT']}]
+) do |faraday|
+
+  faraday.request :aws_signers_v4,
       credentials: Aws::Credentials.new(ENV['ELASTICSEARCH_KEY'], ENV['ELASTICSEARCH_SECRET']),
       service_name: 'es',
       region: ENV['ELASTICSEARCH_REGION']
-    faraday.adapter  Faraday.default_adapter
-  end
 
-  Elasticsearch::Model.client = Elasticsearch::Client.new(transport: transport)
+  faraday.request :json
+  faraday.response :logger 
+  faraday.response :raise_error
 
+  faraday.adapter  Faraday.default_adapter
 end
 
-if Rails.env.development?
-  Elasticsearch::Model.client = Elasticsearch::Client.new(trace: true, log: true)
-end
+Elasticsearch::Model.client = Elasticsearch::Client.new(transport: transport, trace: true, log: true)
