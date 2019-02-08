30163c2ca58a7daf2dd1552b03c93239d9b4c75d
Luke S <luke.sands@cabinetoffice.gov.uk>
2019-02-08 12:03:12 +0000

    Added transport options specifying JSON

--- config/initializers/elasticsearch.rb
+++ config/initializers/elasticsearch.rb
@@ -11,10 +11,10 @@ if Rails.env.production? or Rails.env.staging?
     faraday.adapter  Faraday.default_adapter
   end
 
-  Elasticsearch::Model.client = Elasticsearch::Client.new(transport: transport)
+  Elasticsearch::Model.client = Elasticsearch::Client.new(transport: transport, transport_options: { headers: { accept:   'application/json' }})
 
 end
 
 if Rails.env.development?
-  Elasticsearch::Model.client = Elasticsearch::Client.new(trace: true, log: true)
+  Elasticsearch::Model.client = Elasticsearch::Client.new(trace: true, log: true, transport_options: { headers: { accept:   'application/json' }})
 end
