33f2eaf2515d1e6d25d83c8b28129482c8593a50
Arul <arul@yayaticonsultingltd.com>
2020-02-14 18:24:05 +0000

    Code cleanup

--- app/controllers/api/people_controller.rb
+++ app/controllers/api/people_controller.rb
@@ -9,13 +9,11 @@ module Api
 
       # GET /api/inactive_users.csv?status=inactive&duration=3
    def inactive_users
-        #if super_admin
          @people = Person.inactive_users(params[:status],params[:duration])
          respond_to do |format|
            format.json { render json: @people}
            format.csv { send_data @people.to_csv}
         end
-     #end
    end
 
 
@@ -27,8 +25,5 @@ module Api
       @person = Person.friendly.includes(:groups).find(params[:id])
     end
 
-    def super_admin
-      current_user.present? && current_user.super_admin?
-    end
   end
 end
